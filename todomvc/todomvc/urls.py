40fe8d8e61b95dc8340132ac45036b390e6f66ab
Cuddlemuffin007 <brennonkrogers@gmail.com>
2016-03-16 14:18:14 -0400

    added api resources and endpoints

--- todomvc/todomvc/urls.py
+++ todomvc/todomvc/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 from django.views.generic import TemplateView
+from todo.views import TodoListCreateAPIView, TodoRetrieveUpdateDestroyAPIView
 
 urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name="index.html")),
+    url(r'^api/todos/$', TodoListCreateAPIView.as_view(), name='todo_list_create_api_view'),
+    url(r'^api/todos/(?P<pk>\d+)$', TodoRetrieveUpdateDestroyAPIView.as_view(),
+        name='todo_list_update_destroy_api_view'),
 ]
